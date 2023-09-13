ef14f1843b36573ce0209705b80ed817bbdae5ca
Kilian Lamula <kilian.lamula@etud.univ-jfc.fr>
2023-09-13 09:29:29 +0200

    Commit service observations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ObservationService } from './services/observations.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'NutriApp';
+
+  constructor(private observationService : ObservationService){}
+
+  ngOnInit(){
+    this.observationService.getObservationById("650014107a32ea001909458f").subscribe((res : any)=>{console.log(res)})
+  }
 }
---
+++ src/app/services/observations.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ObservationService {
+
+  constructor(private http: HttpClient) { }
+
+  getObservationById(patientId:string): Observable<any[]> {
+    return this.http.get<any[]>(`https://fhir.alliance4u.io/api/observation?subject.reference=Patient/${patientId}`);
+  }
+
+
+}
