31a441a2fad68a78c648c558a92b22a6833e4c9e
Lise Gauthier <lise-gauthier@live.fr>
2023-09-13 16:25:43 +0200

    Graph IMC

--- src/app/services/observations.service.ts
+++ src/app/services/observations.service.ts
@@ -3,65 +3,75 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ObservationService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getObservationsByPatientId(patientId:string): Observable<any[]> {
-    return this.http.get<any[]>(`https://fhir.alliance4u.io/api/observation?subject.reference=Patient/${patientId}`)
-                    .pipe(
-                      map((res)=>{
-                        let mesuresPatient = []
-                        for( let obs of res ){
-                          let poid
-                          let taille
-                          let alreadyIn = false
-                          //si l'observation est une taille
-                          if(res[0].valueQuantity.unit === 'cm'){
-                            taille = res[0].valueQuantity.value
-                            //on cherche le poid correspondant
-                            for( let obs2 of res){
-                              if(obs.effectiveDateTime === obs2.effectiveDateTime){
-                                for(let mesure of mesuresPatient){
-                                  if(mesure.date === obs.effectiveDateTime){
-                                    alreadyIn = true
-                                  }
-                                }
-                                if(!alreadyIn){
-                                  mesuresPatient.push({taille: taille, poid: obs2.valueQuantity.value, date: obs.effectiveDateTime})
-                                }
-                              }
-                            }
-
-                          }
-                          //si l'observation est un poids 
-                          if(res[0].valueQuantity.unit === 'kg'){
-                            poid = res[0].valueQuantity.value
-                            //on cherche la taille correspondante
-                            for( let obs2 of res){
-                              if(obs.effectiveDateTime === obs2.effectiveDateTime){
-                                //on regarde si la date n'est pas déjà présente dans le tableau
-                                for(let mesure of mesuresPatient){
-                                  if(mesure.date === obs.effectiveDateTime){
-                                    alreadyIn = true
-                                  }
-                                }
-                                if(!alreadyIn){
-                                  mesuresPatient.push({taille: obs2.valueQuantity.value, poid: poid, date: obs.effectiveDateTime})
-                                }
-                              }
-                            }
-                          }
-                        }                        
-                        return mesuresPatient 
-                      })
-                    )
+  getObservationsByPatientId(patientId: string): Observable<any[]> {
+    return this.http
+      .get<any[]>(
+        `https://fhir.alliance4u.io/api/observation?subject.reference=Patient/${patientId}`
+      )
+      .pipe(
+        map((res) => {
+          let mesuresPatient = [];
+          for (let obs of res) {
+            let poids;
+            let taille;
+            let alreadyIn = false;
+            //si l'observation est une taille
+            if (res[0].valueQuantity.unit === 'cm') {
+              taille = res[0].valueQuantity.value;
+              //on cherche le poids correspondant
+              for (let obs2 of res) {
+                if (obs.effectiveDateTime === obs2.effectiveDateTime) {
+                  for (let mesure of mesuresPatient) {
+                    if (mesure.date === obs.effectiveDateTime) {
+                      alreadyIn = true;
+                    }
+                  }
+                  if (!alreadyIn) {
+                    mesuresPatient.push({
+                      taille: taille,
+                      poids: obs2.valueQuantity.value,
+                      date: obs.effectiveDateTime,
+                    });
+                  }
+                }
+              }
+            }
+            //si l'observation est un poids
+            if (res[0].valueQuantity.unit === 'kg') {
+              poids = res[0].valueQuantity.value;
+              //on cherche la taille correspondante
+              for (let obs2 of res) {
+                if (obs.effectiveDateTime === obs2.effectiveDateTime) {
+                  //on regarde si la date n'est pas déjà présente dans le tableau
+                  for (let mesure of mesuresPatient) {
+                    if (mesure.date === obs.effectiveDateTime) {
+                      alreadyIn = true;
+                    }
+                  }
+                  if (!alreadyIn) {
+                    mesuresPatient.push({
+                      taille: obs2.valueQuantity.value,
+                      poids: poids,
+                      date: obs.effectiveDateTime,
+                    });
+                  }
+                }
+              }
+            }
+          }
+          return mesuresPatient;
+        })
+      );
   }
 
-  deleteObservationById(obsId:string): any {
-    return this.http.delete(`https://fhir.alliance4u.io/api/observation/${obsId}`)
+  deleteObservationById(obsId: string): any {
+    return this.http.delete(
+      `https://fhir.alliance4u.io/api/observation/${obsId}`
+    );
   }
-  
 }
